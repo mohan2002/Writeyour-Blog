29c3188ddec1daaf77dcd24cf30a78f762ad29b3
mohan2002 <64457034+mohan2002@users.noreply.github.com>
2022-02-08 09:40:06 +0530

    creating post over

--- website/__init__.py
+++ website/__init__.py
@@ -2,13 +2,41 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
+
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views,url_prefix="/")
+    app.register_blueprint(auth,url_prefix="/")
+    
+    from .models import User,Post
+
+    create_database(app)
+    
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     return app
 
-
+def create_database(app):
+    if not path.exists("webiste/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created database")
     
+# Session what you did in the page last may be in facebook once logged in you can be temporarily store client in f
---
+++ website/views.py
@@ -0,0 +1,30 @@
+
+from flask import Blueprint,render_template,redirect,request,flash, url_for
+from flask_login import login_required, current_user
+from .models import Post
+from . import db
+views = Blueprint("views",__name__)
+
+@views.route('/')
+@views.route('/home')
+@login_required
+def home():
+    posts = Post.query.all()
+    return render_template("Home.html",user=current_user)
+
+
+@views.route("/create-post",methods=['GET','POST'])
+@login_required
+def create_post():
+    if request.method == 'POST':
+        text = request.form.get('text')
+
+        if not text:
+            flash("Post cannot be empty",category='error')
+        else:
+            post = Post(text=text,author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash("Post created!",category="success")
+            return redirect(url_for("views.home"))
+    return render_template('create_post.html',user=current_user)
